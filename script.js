b1161f6d7c956038616801fa2d59e6b2e6da1167
bananacode1234 <92938133+bananacode1234@users.noreply.github.com>
2021-12-17 20:37:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,46 @@ function getDateTime() {
         var now     = new Date();
         var hour    = now.getHours();
         var minute  = now.getMinutes();
-        var second  = now.getSeconds(); 
+        var second  = now.getSeconds();    
+        if(hour > 12) {
+             switch (hour) {
+                     case 13:
+                                hour = 1;
+                                break;
+                     case 14:
+                                hour = 2;
+                                break;
+                     case 15:
+                                hour = 3;
+                                break;
+                     case 16:
+                                hour = 4;
+                                break
+                     case 17:
+                                hour = 5;
+                                break;
+                     case 18:
+                                hour = 6;
+                                break;
+                     case 19:
+                                hour = 7;
+                                break;
+                     case 20:
+                                hour = 8;
+                                break;
+                     case 21:
+                                hour = 9;
+                                break;
+                     case 22:
+                                hour = 10;
+                                break;
+                     case 23:
+                                hour = 11;
+                                break;
+                     case 24:
+                                hour = 12;
+                                break;
+        }
         if(hour.toString().length == 1) {
              hour = '0'+hour;
         }
@@ -11,7 +50,8 @@ function getDateTime() {
         }
         if(second.toString().length == 1) {
              second = '0'+second;
-        }   
+        }
+        }
         var dateTime = hour+':'+minute+':'+second+'s';   
         return dateTime;
     }
