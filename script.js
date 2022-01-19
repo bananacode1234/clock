6fed4a7e3ccd52d98150d245684b64a6e122736b
bananacode1234 <92938133+bananacode1234@users.noreply.github.com>
2022-01-19 15:00:29 -0500

    Cleaned up the code.

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function getDateTime() {
-        var now     = new Date();
-        var hour    = now.getHours();
-        var minute  = now.getMinutes();
-        var second  = now.getSeconds();    
-        if(hour.toString().length == 1) {
-             hour = '0'+hour;
-        }
-        if(minute.toString().length == 1) {
-             minute = '0'+minute;
-        }
-        if(second.toString().length == 1) {
-             second = '0'+second;
-        }
-        var dateTime = hour+':'+minute+':'+second+'s';   
-        return dateTime;
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();    
+    if(hour.toString().length == 1) {
+        hour = '0'+hour;
     }
-
-    setInterval(function(){
-        currentTime = getDateTime();
-        document.getElementById("time").innerHTML = currentTime;
-        document.querySelector("title").innerHTML = currentTime;
-    }, 1000);
+    if(minute.toString().length == 1) {
+        minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }
+    var dateTime = hour+':'+minute+':'+second+'s';   
+    return dateTime;
+}
+setInterval(function(){
+    currentTime = getDateTime();
+    document.getElementById("time").innerHTML = currentTime;
+    document.querySelector("title").innerHTML = currentTime;
+}, 1000);
